dbe8867c77e91f761ed9fc9cada143eb27b55c45
olamide-ibiloye <ibiloyeolamide@gmail.com>
2024-03-20 10:29:51 +0000

    Updated transporter

--- utils/sendEmails.ts
+++ utils/sendEmails.ts
@@ -33,15 +33,26 @@ let transporter = nodemailer.createTransport({
 // Function to send email notification to info@arisebusinessclub.com
 const sendEmailNotification = async (values: ContactFormData) => {
   try {
-    // Send mail with defined transport object
-    let info = await transporter.sendMail({
+    const mailData = {
       from: SENDER,
       to: NOFICATION_RECIPIENT, // List of receivers
       subject: "New Contact Form Submission", // Subject line
       text: `New contact form submission:\n\nName: ${values.name}\nEmail: ${values.email}\nMessage: ${values.message}`,
+    };
+
+    // Send mail with defined transport object
+    let info = await new Promise((resolve, reject) => {
+      transporter.sendMail(mailData, (err, info) => {
+        if (err) {
+          console.error(err);
+          reject(err);
+        } else {
+          resolve(info);
+        }
+      });
     });
 
-    console.log("Email notification sent:", info.messageId);
+    console.log("Email notification sent");
   } catch (error) {
     console.error("Error sending email notification:", error);
   }
@@ -50,15 +61,26 @@ const sendEmailNotification = async (values: ContactFormData) => {
 // Function to send confirmation/response email to sender
 const sendConfirmationEmail = async (email: EmailAddress) => {
   try {
-    // Send mail with defined transport object
-    let info = await transporter.sendMail({
+    const mailData = {
       from: SENDER,
       to: email,
       subject: "Thank you for your inquiry",
       html: EMAIL_TEMPLATE,
+    };
+
+    // Send mail with defined transport object
+    let info = await new Promise((resolve, reject) => {
+      transporter.sendMail(mailData, (err, info) => {
+        if (err) {
+          console.error(err);
+          reject(err);
+        } else {
+          resolve(info);
+        }
+      });
     });
 
-    console.log("Confirmation email sent:", info.messageId);
+    console.log("Confirmation email sent");
   } catch (error) {
     console.error("Error sending confirmation email:", error);
   }
