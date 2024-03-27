6fc0201a435a128a052f7bda30dcd7adad30307a
olamide-ibiloye <ibiloyeolamide@gmail.com>
2024-03-27 17:55:55 +0000

    Mail Pop up form instalation

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,16 +18,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
-        <Script id="mcjs">
-          {`
-          !function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/8e68430ecf8fa289502db0874/83c24030351ca0fcda5449589.js")
-          `}
-        </Script>
-      </head>
       <body className={inter.className}>
         <ThemeProvider enableSystem={false}>
           <main className="flex min-h-screen flex-col items-center justify-between">
+            <Script id="mcjs">
+              {`
+          !function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/8e68430ecf8fa289502db0874/83c24030351ca0fcda5449589.js");
+          `}
+            </Script>
             <NavBar />
             {children}
             <Footer />
